220b34222d1f8d36da7383d974edd84c4b11db64
OleLokison <72652175+OleLokison@users.noreply.github.com>
2020-10-10 15:02:57 +0200

    Übergang desk. zu Git

---
+++ p2.py
@@ -0,0 +1,9 @@
+from threading import Thread, Lock
+from time import time, sleep
+from ctypes import Structure, windll, c_uint, sizeof, byref
+from pynput import mouse, keyboard
+
+
+for i in range(100):
+    windll.user32.mouse_event(0x0001, -5, -5, 0, 0)
+    sleep(0.01)
