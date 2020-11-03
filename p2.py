262a71090da7d79f361a28e7b99456d63f5e136b
OleLokison <8holztisch8@gmx.ch>
2020-11-03 18:23:05 +0100

    3.11.2020-0

--- p2.py
+++ p2.py
@@ -1,6 +1,48 @@
-from PIL import Image
-FilePath = r"C:\Users\8holz\Desktop\Dataset_prot\\"
-FileName = r"2020-11-02-0.png"
+from threading import Thread
+from time import time, sleep
+from ctypes import Structure, windll, c_uint, sizeof, byref
+from pynput import mouse, keyboard
+Hotkey, TriggerKey, TriggerSupportKey, TriggerSupportShutDown=False, True, False, True
 
-im = Image.open(FilePath+FileName)
-im.save(FilePath+"Test.png", dpi=(2560, 1440))
+def TriggerSupport():
+	global TriggerKey, TriggerSupportKey, TriggerSupportShutDown
+	while TriggerSupportShutDown:
+		if TriggerSupportKey:
+			TriggerKey=False
+			for i in range(2):
+				print("Woooooooow")
+				sleep(0.1)
+				windll.user32.mouse_event(0x0002, 0, 0, 0, 0)
+				sleep(0.01)
+				windll.user32.mouse_event(0x0004, 0, 0, 0, 0)
+			TriggerKey=True
+			TriggerSupportKey=False
+
+def on_press(key):
+	global Hotkey
+	print(str(key))
+	try:
+		if key.char=="5":
+			KeyboardListener.stop()
+		if key.char=="6":
+			Hotkey=not(Hotkey)
+	except AttributeError:
+		None
+def on_click(a1,a2,a3,a4):
+	print("click")
+	global Hotkey, TriggerKey, TriggerSupportKey
+	if Hotkey and TriggerKey and a4:
+		TriggerSupportKey=True
+
+TriggerSupportThread = Thread(target=TriggerSupport, daemon=True)
+TriggerSupportThread.start()
+
+MouseListener = mouse.Listener(on_click=on_click)
+MouseListener.start()
+
+with keyboard.Listener(on_press=on_press) as KeyboardListener:
+	KeyboardListener.join()
+
+"""
+will detecten, kann aber nicht weil funktion schon gecallt
+"""
