20df02690284339d4c241fdf331d205b264b2cb0
OleLokison <8holztisch8@gmx.ch>
2020-11-29 23:22:26 +0100

    29.11.2020-2

--- Dataset/MultiplySet.py
+++ Dataset/MultiplySet.py
@@ -6,11 +6,11 @@ from PIL import Image as Pil_image, ImageTk as Pil_imageTk
 import pickle
 import random
 
-DATADIR0 = r"D:\K14\Dataset\K14 Prototyp 572x572\K14 Prototyp 572x572\ds0\img"
-DATADIR1 = r"D:\K14\Dataset\K14 Prototyp 572x572\K14 Prototyp 572x572\ds0\masks_machine"
-DATADIR2 = r"D:\K14\Dataset\K14 Prototyp 572x572\Prep.pickle"
+DATADIR0 = r"C:\Users\8holz\Documents\GitHub\K14 Githubg\Dataset\K14 Prototyp 572x572\K14 Prototyp 572x572\ds0\img"
+DATADIR1 = r"C:\Users\8holz\Documents\GitHub\K14 Githubg\Dataset\K14 Prototyp 572x572\K14 Prototyp 572x572\ds0\masks_machine"
+DATADIR2 = r"C:\Users\8holz\Documents\GitHub\K14 Githubg\Dataset\K14 Prototyp 572x572\Prep.pickle"
 
-def ImCompareGray(Im1, Im2, FigSize):
+def ImCompareGray(Im1, Im2, FigSize=(10,10)):
     f = plt.figure(figsize=FigSize)
     f.add_subplot(1,2, 1)
     plt.imshow(Im1, cmap="gray")
@@ -35,34 +35,48 @@ def MultiplyImages(InputDir0, InputDir1, ImWidth, ImHeight, TestFraction=0.25):
 		Images1,
 		]
 
-def MultImg(Images0, Images1, ShapeTuple):
+def MultImg(Images0, Images1, Multiplications, ShapeTuple):
 	TestImg = random.choice(Images0), random.choice(Images1)
-	if (ShapeTuple[0] > random.choice(TestImg[0]).shape[0] or 
-		ShapeTuple[0] > random.choice(TestImg[1]).shape[0] or 
-		ShapeTuple[1] > random.choice(TestImg[0]).shape[1] or 
-		ShapeTuple[1] > random.choice(TestImg[1]).shape[1]):
+	if (ShapeTuple[0] > TestImg[0].shape[0] or 
+		ShapeTuple[0] > TestImg[1].shape[0] or 
+		ShapeTuple[1] > TestImg[0].shape[1] or 
+		ShapeTuple[1] > TestImg[1].shape[1]):
 		print("ShapeTuple is to Big for Operation")
 		return
 	PosPixels = {}
 	NewImages0 = []
 	NewImages1 = []
 	PosArea = {
-		"xmin":ShapeTuple[0], 
-		"ymin":ShapeTuple[1]}
-	for i in range(len(Images1)):
-		rows,cols = Images1[i].shape
-		break
+		"xmax":TestImg[0].shape[0]-ShapeTuple[0], 
+		"ymax":TestImg[0].shape[1]-ShapeTuple[1]}
+	for k in range(len(Images1)):
+		rows,cols = Images1[k].shape
 		for i in range(rows):
 			for j in range(cols):
-				if Images1[i][i,j] == 1:
+				if Images1[k][i,j] == 1:
 					PosPixels.update({i:j})
-		Pickedi, Pickedj = random.choice(list(PosPixels.items()))
-		if Pickedi < ShapeTuple[0]:
-			Pickedi = ShapeTuple[0]
-		if Pickedj < ShapeTuple[1]:
-			Pickedj = ShapeTuple[1]
-		#
-		NewImages0.append(Images0[y:y+h, x:x+w])
+		for m in range(Multiplications):
+			maxx, maxy = random.choice(list(PosPixels.items()))
+			minx, miny = maxx - ShapeTuple[0], maxy - ShapeTuple[1]
+			if minx < 0:
+				minx = 0
+			if miny < 0:
+				miny = 0
+			if maxx >  TestImg[0].shape[0]-ShapeTuple[0]:
+				maxx = TestImg[0].shape[0]-ShapeTuple[0]
+			if maxy > TestImg[0].shape[1]-ShapeTuple[1]:
+				maxy = TestImg[0].shape[1]-ShapeTuple[1]
+			x = random.randrange(minx, maxx)
+			y = random.randrange(miny, maxy)
+			NewImage1 = (Images1[k][x:x+ShapeTuple[0], y:y+ShapeTuple[1]])
+			NewImage0 = (Images0[k][x:x+ShapeTuple[0], y:y+ShapeTuple[1]])
+			NewImages1.append(NewImage1)
+			NewImages0.append(NewImage0)
+	return [NewImages0, NewImages1]
+
 
 dat = MultiplyImages(DATADIR0, DATADIR1, 572, 572)
-MultImg(dat[0], dat[1], (100, 100))
+dat1 = MultImg(dat[0], dat[1], 1, (100, 100))
+print(dat1)
+for i in range(1):
+	ImCompareGray(dat1[0][i], dat1[1][i])
