a9ebd9ec9f767c230bcef73681c4a0ec855b762c
OleLokison <72652175+OleLokison@users.noreply.github.com>
2020-12-01 21:46:09 +0100

    Revert "30.11.2020-0"
    This reverts commit d72bad259b5d8add31b059c5b0648feb92a5d9d2.

--- p3.py
+++ p3.py
@@ -3,10 +3,4 @@ from matplotlib import pyplot as plt
 import random
 a = (1, 2)
 
-
-try:
-    a = random.randrange(0, 0)
-except ValueError:
-    print("hey")
-    a = random.randrange(0, 10)
-print(a)
+print(a[1])
