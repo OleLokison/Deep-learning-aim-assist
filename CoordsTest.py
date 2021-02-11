7c6531b9085ad237acde77701aa478c607ecac81
OleLokison <8holztisch8@gmx.ch>
2021-02-11 12:49:30 +0100

    11.02.2021-1

--- CoordsTest.py
+++ CoordsTest.py
@@ -49,3 +49,60 @@ cv2.waitKey()
 """
 
 ImCompareGray(ims[1][i], image)
+
+"""
+import os
+import pickle
+from win32gui import FindWindow, SetForegroundWindow, GetClientRect, ClientToScreen
+import pyautogui
+import cv2
+import numpy as np
+import tkinter as tk
+from PIL import Image, ImageTk
+from matplotlib import pyplot as plt
+from ctypes import windll
+from math import floor, ceil
+import win32api
+import time
+
+def Coords(Pred, small):
+	#cv2, Predicitons.pickle[2]=small
+	Pred = (255 - cv2.inRange(np.array(Pred * 255, dtype = np.uint8), 0, 160)).astype(np.float32)
+	small = np.array(small * 255, dtype = np.uint8).astype(np.float32)
+	result = cv2.matchTemplate(Pred, small, cv2.TM_SQDIFF_NORMED)
+	mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+	MPx,MPy = mnLoc
+	#x and y may flipped
+	return (MPx+2-256, MPy+2-256)
+	#return (MPx, MPy)
+
+def ImCompareGray(Im1, Im2, FigSize=(10,10)):
+    f = plt.figure(figsize=FigSize)
+    f.add_subplot(1,2, 1)
+    plt.imshow(Im1, "gray")
+    f.add_subplot(1,2, 2)
+    plt.imshow(Im2)
+    plt.show(block=True)
+
+Template = pickle.load(open(r"D:\K14\Dataset\Predicitions.pickle", "rb"))[44]
+ims = pickle.load(open(r"D:\K14\Dataset\TestImages.pickle", "rb"))
+
+print(Template.shape)
+
+
+i = 8
+
+c = Coords(ims[1][i], Template)
+print(c)
+print(c[0]+256, c[1]+256)
+
+image = cv2.rectangle(ims[0][i], (c[0]+256, c[1]+256), (c[0]+256+2, c[1]+256+2), (0,255,0), 2)
+"""
+"""
+cv2.imshow("image", image)
+cv2.waitKey()
+"""
+"""
+ImCompareGray(ims[1][i], image)
+
+"""
--- p1.py
+++ p1.py
@@ -11,40 +11,55 @@ from ctypes import windll
 from math import floor, ceil
 import win32api
 import time
-#260, 260
-def ImCompareGray(Im1, Im2, FigSize=(10,10)):
-    f = plt.figure(figsize=FigSize)
-    f.add_subplot(1,2, 1)
-    plt.imshow(Im1, "gray")
-    f.add_subplot(1,2, 2)
-    plt.imshow(Im2)
-    plt.show(block=True)
+from threading import Thread
 
-Template = pickle.load(open(r"D:\K14\Dataset\Predicitions.pickle", "rb"))
-ims = pickle.load(open(r"D:\K14\Dataset\TestImages.pickle", "rb"))
-image = ims[1][8]
-image = (255 - cv2.inRange(np.array(image * 255, dtype = np.uint8), 0, 160)).astype(np.float32)
+class MouseControlV4(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        None
+    def run(self):
+        #32 directions, 512 distances, 1 Iteration = 0.015
+        global distx, disty, strength, Exceptions, MaxIterations
+        Ldistx, Ldisty = distx, disty
+        while StopKey:
+            if Ldistx != distx or Ldisty != disty:
+                t = time.time()
+                Ldistx = distx
+                Ldisty = disty
+                Lstrength = strength
+                LExceptions = Exceptions
+                LMaxIterations = MaxIterations
+                x = distx*strength
+                y = disty*strength
+                #print(x,y)
+                if x not in Exceptions or y not in Exceptions:
+                    if min(x,y)<=1:
+                        windll.user32.mouse_event(0x0001, int(x), int(y), 0, 0)
+                    else:
+                        if min(x,y)>MaxIterations:
+                            Iterations = 6
+                        else:
+                            Iterations=ceil(min(x,y))
+                        x1 = floor(x/(Iterations-1))
+                        y1 = floor(y/(Iterations-1))
+                        for i in range(Iterations-1):
+                            windll.user32.mouse_event(0x0001, x1, y1, 0, 0)
+                            #time.sleep(0.005)
+                        windll.user32.mouse_event(0x0001, int(x)-(x1*Iterations-1), int(y)-(y1*Iterations-1), 0, 0)
+                print(time.time()-t)
+            time.sleep(0.05)
+
+distx, disty = 0, 0
+strength = 0.1
+Exceptions = [-76.2, 33.3, -35.699999999999996, 77.0, -88.5, -127.0, -97.5, 32.5, -21.5, -68.0, -254]
+MaxIterations = 3
+StopKey = True
+
+inst1 = MouseControlV4()
+inst1.daemon = True
+inst1.start()
 
 
-for i in Template:
-	print(i.shape)
+distx, disty = 10, 100
 
-
-p = 260
-size = 5
-t = []
-
-Template = np.array(Template[0] * 255, dtype = np.uint8).astype(np.float32)
-for i in range(1, 46):
-	ima = image[p:p+size+i, p:p+size+i]
-	ima = ima.reshape(size+i, size+i, 1)
-	t.append(np.array(ima * 255, dtype = np.uint8).astype(np.float32))
-for i in t:
-	print(i.shape)
-
-
-pickle.dump(t, open(r"D:\K14\Dataset\Predicitions.pickle", "wb"))
-
-"""
-war beim erstellen von neuen Templates, richtig frustrierend das richtige format zu bekommen
-"""
+time.sleep(1)
